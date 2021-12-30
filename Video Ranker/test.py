6b81e26b16104c9a0580a8be10c7eafe007e43cf
Daniel Hernandez <dhrod.2003@gmail.com>
2021-12-30 01:02:06 -0500

    video ranking done

---
+++ Video Ranker/test.py
@@ -0,0 +1,9 @@
+marks = {}
+
+for i in range(3):
+    student_name = input("Enter student's name: ")
+    student_mark = input("Enter student's mark: ")
+
+    marks[student_name] = student_mark
+
+print(marks)
--- Video Ranker/videorank.py
+++ Video Ranker/videorank.py
@@ -4,44 +4,47 @@
 # Program description: User enter various and their amount of views, show the total amount of 
 # views and a ranking from most to least watched videos.
 
+
 # Welcome user
 
 
 print("Welcome to the video ranking calculator")
 
+
 # Ask the user for the number of videos
 
 
 def GetVideos():
-    global videotitle
-    global videorank
     global videoNumber
     global totalViews
     global videocounter
+    global title_views 
+    global title
+    global views
+    title_views = {}
+    rankNumber = []
+    rankholder = 0
     totalViews = 0
-    videorank = []
-    videotitle = []
     videoNumber = int(input("How many videos do you want to analyze? "))
     for video in range(1, videoNumber + 1):
         title = input("Enter the name of the video " + str(video) + " ")
-        videotitle.append(title)
         views = int(input("Enter the number of views for video " + str(video) + " "))
-        videorank.append(views)
         totalViews += views
-
-
-def showResults():
-    import itertools
-    global totalViews
-    global videorank
+        rankholder += 1
+        rankNumber.append(rankholder)
+        title_views[title] = views
+        
+    ranking = dict(sorted(title_views.items(), key=lambda x: x[1], reverse=True))
     avgViews = totalViews / videoNumber
-    print("The total amount of views is ", sum(videorank))
-    print("The average amount of views is", avgViews)
-    videorank.reverse()
-    print("Video raking:")
-    for (number, title, video) in itertools.zip_longest(range(1, videoNumber + 1), videotitle, videorank):
-        print(number, title, video, "views")
+    print("\n The total amount of views is ", totalViews)
+    print(" The average amount of views is", avgViews)
+    print(" Video ranking:")
+    for rank, (key,value) in zip(rankNumber, ranking.items()):
+        print(" " + str(rank) + ". " + key + " ---> " + str(value), "views")
 
+
+
+
+    
 GetVideos()
-showResults()
         
